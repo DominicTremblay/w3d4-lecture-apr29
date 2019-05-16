e6392b62d57a9b5ff8579999dcd932d4fcaed125
Dominic Tremblay <dominictremblay24@gmail.com>
2019-05-16 11:31:51 -0400

    final demo apr29

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,96 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  // Get the info from json
+  // Create an Ajax request to get the json of the quotes
+  // Array of objects
+
+  const request = (options, cb) => {
+    $.ajax(options)
+      .done(response => {
+        cb(response);
+      })
+      .fail(err => console.log('Error', err))
+      .always(() => console.log('Request completed.'));
+  };
+
+  // create a quote HTML element
+
+  const createQuoteEl = quoteObj => {
+    const quoteEl = `<div class="card">
+  <div class="card-header" id="${quoteObj._id}">
+    <h5 class="mb-0">
+      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#${
+        quoteObj._id
+      }" aria-expanded="true" aria-controls="5c93486deb1896280bf3b720" style="visibility: visible;">${
+      quoteObj.quote
+    }</button>
+      <div>
+        <form class="quote-update-frm" style="visibility: hidden;">
+          <input type="text" name="editquote" class="edit-quote" value="${
+            quoteObj.quote
+          }">
+          <input type="submit" class="btn-small btn-primary" value="Update">
+        </form>
+      </div>
+    </h5>
+
+    <span>
+      <form class="quote-edit-frm"><input type="submit" class="btn btn-secondary btn-sm" value="Edit"></form>
+      <form class="quote-delete-frm"><input type="submit" class="btn btn-secondary btn-sm" value="Delete"></form>
+    </span>
+
+  </div>
+</div>`;
+    return quoteEl;
+  };
+
+  // Loop throught the array of quotes
+  // Create all the quote Element
+  // Add each one to the DOM
+  const renderQuotes = quoteArr => {
+    $.each(quoteArr, (index, quote) => {
+      // Add the elements to the DOM with append
+      $('#quote-list').prepend(createQuoteEl(quote));
+    });
+  };
+
+  const loadQuotes = () => {
+    const reqOptions = {
+      method: 'GET',
+      url: '/quotes',
+      dataType: 'json',
+    };
+
+    // Trigger the Ajax request using reqOptions
+    request(reqOptions, quoteArr => {
+      // Callback is how we want to deal with the result
+      renderQuotes(quoteArr);
+    });
+  };
+
+  // catch the onsubmit event of the form with id add-quote-frm
+  $('#add-quote-frm').on('submit', function(event) {
+    // Stop the normal submission of the form
+    event.preventDefault();
+
+    // Extract the information from the form
+    const quoteContent = $(this)
+      .find('#quote')
+      .val();
+    console.log(quoteContent);
+
+    // Create the Ajax post request
+    const reqOptions = {
+      method: 'POST',
+      url: '/quotes',
+      data: { quote: quoteContent },
+    };
+
+    request(reqOptions, quote => {
+      // add my quoteElement to the list
+      renderQuotes([quote]);
+    });
+  });
+
+  // Load all the quotes
+  loadQuotes();
+});
--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const MONGODB_URI =
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// use static files from public folder
 app.use(express.static('public'));
 
 let db = null;
@@ -25,6 +27,45 @@ MongoClient.connect(MONGODB_URI, (err, mongoDb) => {
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
   db = mongoDb;
+});
+
+// List the quotes
+app.get('/quotes', (req, res) => {
+  // Do the DB request to get all the quotes
+
+  db.collection('quotes')
+    .find()
+    .toArray()
+    .then(quotes => res.json(quotes));
+});
+
+// Show only one quote
+app.get('/quotes/:id', (req, res) => {});
+
+// Create a quote
+app.post('/quotes', (req, res) => {
+  // Ex traction the quote content for req.body
+  console.log('req.body:', req.body);
+  const { quote } = req.body;
+  // Create a new quote in the db
+  db.collection('quotes')
+    .insertOne({
+      quote: quote,
+      comments: [],
+    })
+    .then(result => res.json(result.ops[0]));
+});
+
+// Update an existing quote
+app.put('/quotes/:id', (req, res) => {
+  // get the id from req.params
+  // retrieve the quote in the DB with that id
+  // Update the quote in the db
+});
+
+// Delete an existing quote
+app.delete('/quotes/:id', (req, res) => {
+  // delete the quote with that id in the db
 });
 
 app.listen(port, () => {
