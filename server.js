147b22350e0665729f1f3a7b8277aea140f76e7e
Dominic Tremblay <dominictremblay24@gmail.com>
2019-05-16 04:18:29 -0400

    demo setup ready

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/movieQuotes';
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
@@ -24,54 +25,6 @@ MongoClient.connect(MONGODB_URI, (err, mongoDb) => {
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
   db = mongoDb;
-});
-
-// Read
-app.get('/quotes', (req, res) => {
-  db.collection('quotes')
-    .find()
-    .toArray()
-    .then(quotes => res.json(quotes))
-    .catch(err => console.log('Error: ', err));
-});
-
-app.get('/quotes/:id', (req, res) => {
-  const { id } = req.params;
-  db.collection('quotes')
-    .findOne({ _id: ObjectID(id) })
-    .then(quote => res.json(quote))
-    .catch(err => console.log('Error: ', err));
-});
-
-app.post('/quotes', (req, res) => {
-  console.log(req.body);
-  const { quoteContent } = req.body;
-  db.collection('quotes')
-    .insertOne({ quote: quoteContent, comments: [] })
-    .then(result => {
-      res.json(result.ops[0]);
-    })
-    .catch(err => console.log('Error:', err));
-});
-
-app.put('/quotes/:id', (req, res) => {
-  const { quoteContent } = req.params;
-  const { id } = req.params;
-  console.log(req.body, req.params);
-  db.collection('quotes')
-    .updateOne({ _id: ObjectID(id) }, { $set: { quote: quoteContent } })
-    .then(result => res.send(quoteContent))
-    .catch(err => console.log(`Error: ${err}`));
-});
-
-app.delete('/quotes/:id', (req, res) => {
-  console.log('DELETE...');
-  const { id } = req.params;
-
-  db.collection('quotes')
-    .deleteOne({ _id: ObjectID(id) })
-    .then(result => res.send(result))
-    .catch(err => res.send(err));
 });
 
 app.listen(port, () => {
